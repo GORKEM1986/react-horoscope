bf0ad9a8aba39fca4975812989f261cdcb20a462
GORKEM1986 <aslanakbey7106@gmail.com>
2023-02-20 14:19:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.scss';
+import Header from "./components/Header/Header"
+import Navbar from './components/Navbar/Navbar';
+import Main from './components/Main/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      
+      <Header/>
+
+      <Main/>
+     
+    </div>
+  );
+}
+
+export default App;
